346dc9533a47cbf7788acdccd6c594d07bd661b0
luis jimenez <eve_ghetto@hotmail.com>
2023-09-14 15:57:01 -0500

    Seed Db Added

---
+++ PactiaPerson.API/Data/SeedDb.cs
@@ -0,0 +1,35 @@
+﻿using PactiaPerson.Shared.Entities;
+
+namespace PactiaPerson.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        //se hace la inyeccion para la base de datos
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        // se crea el metodo para inicializar la base de datos con algunos registros
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckPersonAsync();
+        }
+
+        //este metodo valida si hay datos en la bd sino los crea
+        private async Task CheckPersonAsync()
+        {
+            if(!_context.Persons.Any()) 
+            {
+                _context.Persons.Add(new Person { Cedula = "9034543468", Nombre = "Enrique", Apellido = "Benitez" });
+                _context.Persons.Add(new Person { Cedula = "7834562309", Nombre = "Camila", Apellido = "Ordoñez" });
+                _context.Persons.Add(new Person { Cedula = "2145358725", Nombre = "Lorena", Apellido = "Moncada" });
+                _context.Persons.Add(new Person { Cedula = "1739347653", Nombre = "JR", Apellido = "Lopez" });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- PactiaPerson.API/Program.cs
+++ PactiaPerson.API/Program.cs
@@ -11,7 +11,23 @@ builder.Services.AddSwaggerGen();
 //conexion a la bd 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=SQLConnection"));
 
+//inicializador del seeder
+builder.Services.AddTransient<SeedDb>();
+
 var app = builder.Build();
+
+SeedData(app);
+
+//debido a que no se puede inyectar por constructor debemos crear el metodo para implementar el seeder
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory= app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
